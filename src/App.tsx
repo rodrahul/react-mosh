6cf279f41cff77e071fbee2e83ebe863c45dcd7d
Rahul Rode <rod.rahul@gmail.com>
2024-10-07 09:51:12 -0400

    Error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
+// import "@mantine/core/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FetchData from "./components/use-effect/FetchData";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     // <MantineProvider>
       <div className="mx-auto max-w-md p-4">
-        <FetchData></FetchData>
+      <FetchData></FetchData>
       </div>
     // </MantineProvider>
   );
--- src/components/use-effect/FetchData.tsx
+++ src/components/use-effect/FetchData.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Alert from "../Alert";
 
 interface User {
   id: number;
@@ -8,15 +9,18 @@ interface User {
 
 const FetchData = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
   }, []);
 
   return (
     <>
+      {error && <Alert message={error} type="error"></Alert>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
