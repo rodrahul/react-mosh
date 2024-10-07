630e6533ab9acce67439e5a40429d936d0243aa8
Rahul Rode <rod.rahul@gmail.com>
2024-10-07 10:09:25 -0400

    Adding loading spinner

--- src/components/use-effect/FetchData.tsx
+++ src/components/use-effect/FetchData.tsx
@@ -10,19 +10,25 @@ interface User {
 const FetchData = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-
+    setIsLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
+        setIsLoading(false);
         setError(err.message);
-      });
+      })
+      .finally(() => setIsLoading(false));
 
     return () => controller.abort();
   }, []);
@@ -30,6 +36,7 @@ const FetchData = () => {
   return (
     <>
       {error && <Alert message={error} type="error"></Alert>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
