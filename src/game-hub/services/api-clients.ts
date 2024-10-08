235bc3819ec7018a2470a0e39633d5b207d6839a
Rahul Rode <rod.rahul@gmail.com>
2024-10-08 16:48:57 -0400

    api key using env

--- src/game-hub/services/api-clients.ts
+++ src/game-hub/services/api-clients.ts
@@ -1,10 +1,11 @@
 import axios, { CanceledError } from "axios";
 
+const API_KEY = import.meta.env.VITE_API_KEY
 
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: ""
+    key: `${API_KEY}`
   }
 })
 
