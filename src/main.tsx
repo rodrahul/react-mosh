64fde5b3da1d79acdfccdec5c8d66fd5dd1c288c
Rahul Rode <rod.rahul@gmail.com>
2024-10-15 09:43:47 -0400

    adds ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./game-hub/theme.ts";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           initialColorMode={theme.config.initialColorMode}
         ></ColorModeScript>
         <App />
+
+        <ReactQueryDevtools />
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>,
