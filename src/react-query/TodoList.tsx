4a2df13db4832fb99db910ea03ed188e64cb3484
Rahul Rode <rod.rahul@gmail.com>
2024-10-15 08:52:59 -0400

    Handlig errors in react query

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -14,7 +14,7 @@ const TodoList = () => {
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
 
-  const { data: todos } = useQuery({
+  const { data: todos, error } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
   });
@@ -30,7 +30,7 @@ const TodoList = () => {
   //     .catch((err) => setError(err));
   // }, []);
 
-  // if (error) return <p>{error}</p>;
+  if (error) return <p>{error.message}</p>;
 
   return (
     <ul className="list-group">
